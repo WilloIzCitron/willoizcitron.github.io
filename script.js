85e681863d2cbbe874bf6372db20ccb48fae066c
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2020-10-26 10:38:22 +0700

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+var r_text = new Array();
+r_text[0] = "this person is a indie dev";
+r_text[1] = "Insert Text Here"
+r_text[2] = "Coca cola espuma"
+r_text[3] = "also try Username601"
+
+var i = Math.floor(4 * Math.random())
+
+document.getElementById("spam").innerHTML = r_text[i];
